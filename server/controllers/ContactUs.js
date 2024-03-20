694f96eefbceff05204ec8109e9e79462cac8ecd
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-03-20 13:13:40 +0530

    Implement controller for contact form submission

---
+++ server/controllers/ContactUs.js
@@ -0,0 +1,28 @@
+const { contactUsEmail } = require("../mail/template/contactFormTemplate");
+const mailSender = require("../utils/mailSender");
+
+exports.contactUsController = async(req, res) =>{
+    const { email, firstname, lastname, message, phoneNo, countrycode } = req.body;
+    console.log(req.body);
+
+    try{
+        const emailRes = await mailSender(
+            email, 
+            "your Data send successfully!",
+            contactUsEmail(email, firstname, lastname, message, phoneNo, countrycode )
+        );
+        console.log("email res: ", emailRes);
+        
+        return res.status(200).json({
+            success: true,
+            message: "Email send successfully!",
+        });
+    }
+    catch(error){
+        console.error("Error occurred while sending email:", error);
+        return res.status(500).json({
+            success: false,
+            message: error.message || "Error occurred while sending email",
+        });
+    }
+};
