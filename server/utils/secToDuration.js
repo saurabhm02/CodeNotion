e1d5d556219c4a9353ae81997b0b9d696e04ce7f
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-03-27 02:10:47 +0530

    implement logic for converting sec to duration

---
+++ server/utils/secToDuration.js
@@ -0,0 +1,17 @@
+const convertSecondsToDuration = (totalSeconds) =>{
+    const hours = Math.floor( totalSeconds / 3600 );
+    const minutes = Math.floor( (totalSeconds % 3600) / 60 );
+    const seconds = Math.floor( (totalSeconds % 60) % 60 );
+
+    if( hours > 0 ){
+        return `${hours}h ${minutes}m`
+    }
+    else if(minutes > 0){
+        return `${minutes}m ${seconds}s`
+    }
+    else{
+        return `${seconds}s`
+    }
+};
+
+module.exports = convertSecondsToDuration;
