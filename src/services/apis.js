85069c9fd232352c33537af5371962e9bde6a9d8
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-21 17:08:49 +0530

    immplemennt services for connecting backend to fronted

---
+++ src/services/apis.js
@@ -0,0 +1,21 @@
+
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const endpoints = {
+    SENDOTP_API: BASE_URL + "/auth/sendotp",
+    SIGNUP_API: BASE_URL + "/auth/signup",
+    LOGIN_API: BASE_URL + "/auth/login",
+    RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
+    RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
+}
+
+// CONTACT-US API
+export const contactusEndpoint = {
+    CONTACT_US_API: BASE_URL + "/reach/contact",
+}
+
+
+export const categories = {
+    CATEGORIES_API: BASE_URL + "/course/getAllCategories",
+}  
