89232779a06684b2102462144f3f056a57918356
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-22 23:11:26 +0530

    define profile api endpoints

--- src/services/apis.js
+++ src/services/apis.js
@@ -20,6 +20,14 @@ export const contactusEndpoint = {
     CONTACT_US_API: BASE_URL + "/reach/contact",
 }
 
+// PROFILE ENDPOINTS
+export const profileEndpoints = {
+    GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
+    GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
+    GET_INSTRUCTOR_DATA_API: BASE_URL + "/profile/instructorDashboard",
+}
+  
+
 
 export const categories = {
     CATEGORIES_API: BASE_URL + "/course/getAllCategories",
