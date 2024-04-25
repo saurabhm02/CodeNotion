958ff279b357c43d557da2182682f68a7b659236
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-26 02:06:57 +0530

    navbar updated

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -53,7 +53,7 @@ function Navbar() {
 
   return (
     <div
-      className={` bg-richblack-900 flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+      className={` bg-richblack-900 flex h-14 items-center justify-around border-b-[1px] border-b-richblack-700 ${
         location.pathname !== "/" ? "bg-richblack-800" : ""
       } transition-all duration-200`}
     >
@@ -63,6 +63,7 @@ function Navbar() {
           <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
         </Link>
         {/* Navigation links */}
+        <div className="flex items-center gap-4">
         <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
             {NavbarLinks.map((link, index) => (
@@ -72,7 +73,7 @@ function Navbar() {
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 ${
                         matchRoute("/catalog/:catalogName")
-                          ? "text-yellow-25"
+                          ? "text-[#FD7014]"
                           : "text-richblack-25"
                       }`}
                     >
@@ -112,7 +113,7 @@ function Navbar() {
                     <p
                       className={`${
                         matchRoute(link?.path)
-                          ? "text-yellow-25"
+                          ? "text-[#FD7014]"
                           : "text-richblack-25"
                       }`}
                     >
@@ -130,7 +131,7 @@ function Navbar() {
             <Link to="/dashboard/cart" className="relative">
               <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
               {totalItems > 0 && (
-                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-[#603d1e]">
                   {totalItems}
                 </span>
               )}
@@ -151,6 +152,7 @@ function Navbar() {
             </Link>
           )}
           {token !== null && <ProfileDropdown />}
+        </div>
         </div>
         <div className='mr-4 md:hidden' >
           <GiHamburgerMenu
